064eca6eda80242cd3f57d3b85bd400f66759c65
R-L-Rayford <rebecca.rayford@yahoo.com>
2019-08-13 15:10:19 -0400

    added new class for the tip options

--- BillSplitter.java
+++ BillSplitter.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import javax.swing.*;
 
 public class BillSplitter {
   public static void main(String[] arg){
@@ -7,27 +8,40 @@ public class BillSplitter {
     float tipPercentage = 0.0f;   // holds amount for tip
     float totalAfterTip = 0.0f;
     float amountEachOwes = 0.0f;
+    
+    JFrame personAmount = new JFrame();
+    JFrame total = new JFrame();
 
     // create scanner for user input
     Scanner input = new Scanner(System.in);
 
     // get the number of people splitting the bill
-    System.out.println("How many people are in your party?");
-    numOfPeopleInParty = input.nextInt();   // reads user input
+    //System.out.println("How many people are in your party?");
+    //numOfPeopleInParty = input.nextInt();   // reads user input
+    
+    String s1 = JOptionPane.showInputDialog(personAmount,"How many people are in your party?");
+    int i = Integer.parseInt(s1);
+    numOfPeopleInParty = i;
 
     // test Scanner
     System.out.println("Party size is: "+numOfPeopleInParty);
     
     // get the total amount of the bill
-    System.out.println("What is the total of the bill?");
-    billTotal = input.nextFloat();
+    //System.out.println("What is the total of the bill?");
+    //billTotal = input.nextFloat();
+    
+    String s2 = JOptionPane.showInputDialog(total,"What is the total of the bill?");
+    int ii = Integer.parseInt(s2);
+    billTotal = ii;
     
     // test scanner
     System.out.println("Bill total is: "+billTotal);
     
     // get amount user want to tip
-    System.out.println("How much would you like to tip?");
-    tipPercentage = input.nextFloat();
+    //System.out.println("How much would you like to tip?");
+    //tipPercentage = input.nextFloat();
+    
+   new TipOptionPaneClass();
     
     // add tip amount to total
     totalAfterTip = billTotal + tipPercentage;
